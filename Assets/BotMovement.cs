746fca6c53de8483c0da1718fa9c37d159f04cff
Josef Utbult <josutb-7@student.ltu.se>
2020-05-10 00:08:16 +0200

    Add bot movement

---
+++ Assets/BotMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotMovement : MonoBehaviour
+{
+	private Vector3 direction;
+	private float thrust = .1f;
+	private float velocityThreshold = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+    	GetComponent<Rigidbody>().AddForce(direction * thrust);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float velocity = Mathf.Sqrt(Mathf.Pow(GetComponent<Rigidbody>().velocity[0], 2) + Mathf.Pow(GetComponent<Rigidbody>().velocity[2], 2));
+
+    	if(velocity < velocityThreshold){
+	    	GetComponent<Rigidbody>().AddForce(direction * thrust);
+    	}
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        direction = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+    	GetComponent<Rigidbody>().AddForce(direction * thrust);
+    }
+}
