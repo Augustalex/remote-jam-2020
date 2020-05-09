af0ac3f26cff2527085aa7dcae791bd4543f02e0
August Alexandersson <augustalexandersson@gmail>
2020-05-10 01:49:49 +0200

    Added game timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private double _startTime = Time.time;
+    private Text _text;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        _text.text = "You've survived for " + Math.Truncate(Time.time - _startTime) + " seconds";
+    }
+}
