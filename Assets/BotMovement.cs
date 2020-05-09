4304f9d247c443ad2898645dc9b31b01abf892a0
August Alexandersson <augustalexandersson@gmail.com>
2020-05-10 00:58:16 +0200

    ...

--- Assets/BotMovement.cs
+++ Assets/BotMovement.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class BotMovement : MonoBehaviour
 {
 	private Vector3 direction;
-	private float thrust = .1f;
-	private float velocityThreshold = 1.0f;
-    // Start is called before the first frame update
+	private float thrust = .25f;
+	private float velocityThreshold = 1f;
+
+	private Rigidbody _rigidbody;
+
+	// Start is called before the first frame update
+    private void Awake()
+    {
+	    _rigidbody = GetComponentInChildren<Rigidbody>();
+    }
+
     void Start()
     {
         direction = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
-    	GetComponent<Rigidbody>().AddForce(direction * thrust);
+    	_rigidbody.AddForce(direction * thrust);
     }
 
     // Update is called once per frame
     void Update()
     {
-    	float velocity = Mathf.Sqrt(Mathf.Pow(GetComponent<Rigidbody>().velocity[0], 2) + Mathf.Pow(GetComponent<Rigidbody>().velocity[2], 2));
+    	float velocity = Mathf.Sqrt(Mathf.Pow(_rigidbody.velocity[0], 2) + Mathf.Pow(_rigidbody.velocity[2], 2));
 
     	if(velocity < velocityThreshold){
-	    	GetComponent<Rigidbody>().AddForce(direction * thrust);
+	    	_rigidbody.AddForce(direction * thrust);
     	}
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collider)
     {
+	    Debug.Log("COLLISION!");
         direction = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
-    	GetComponent<Rigidbody>().AddForce(direction * thrust);
+    	_rigidbody.AddForce(direction * thrust);
     }
 }
