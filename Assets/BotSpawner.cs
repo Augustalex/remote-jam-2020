795d0efbbab01b28d3fcb2e967f49ebd8cade014
August Alexandersson <augustalexandersson@gmail>
2020-05-10 02:21:25 +0200

    ...

---
+++ Assets/BotSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Users;
+
+public class BotSpawner : MonoBehaviour
+{
+    public GameObject BotTemplate;
+    private GameConfig _gameConfig;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _gameConfig = GameObject.FindGameObjectWithTag("GameConfig").GetComponent<GameConfig>();
+
+        for (var i = 0; i < _gameConfig.difficultyLevel; i++)
+        {
+            Instantiate(BotTemplate, transform.position, transform.rotation);
+        }
+    }
+}
